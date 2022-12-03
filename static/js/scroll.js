675c8838b2464db846e25113bbf86c81a145abac
Ko Nak-yean <59277499+nakyeonko3@users.noreply.github.com>
2022-12-04 04:18:47 +0900

    update css view

---
+++ static/js/scroll.js
@@ -0,0 +1,5 @@
+document.querySelector(".abutton").addEventListener('click', () => {
+  //   window.scrollTo({ left: 0, top: 0, behavior: 'smooth' });
+  console.log('click');
+  window.scrollTo({ left: 0, top: 300, behavior: 'smooth' });
+});
